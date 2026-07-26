52d0265900ed91b34d912c6231755cb38f7de77c
Surendra Singh <surendrasinghrathore13057@gmail.com>
2026-07-26 14:36:12 +0530

    server from node completed

--- webserver/server-node.js
+++ webserver/server-node.js
@@ -4,9 +4,19 @@ const hostname = '127.0.0.1'
 const port= 3000
 
 const server=http.createServer((req,res)=>{
-    res.statusCode = 200
-    res.setHeader('Content-Type','text/plain')
-    res.end("hello ice tea")
+    if (req.url ==='/') {
+        res.statusCode = 200
+        res.setHeader('Content-Type','text/plain')
+        res.end("hello ice tea")
+    }else if(req.url==='/ice-tea'){
+        res.statusCode = 200
+        res.setHeader('Content-Type','text/plain')
+        res.end("hey thanks for visiting it")
+    }else{
+        res.statusCode = 404
+        res.setHeader('Content-Type','text/plain')
+        res.end("404 not found")
+    }
 })
 
 server.listen(port,hostname,()=>{
